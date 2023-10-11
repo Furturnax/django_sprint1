e9f4bf5c5e8d07d4bc89119626442e63820a4e9a
Andrew Fedorchenko <furturnax@gmail.com>
2023-10-11 23:47:31 +0300

    Done pages 'about' and 'rules'. Fix general urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,7 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('blog.urls', namespace='index')),
-    path('posts/', include('blog.urls', namespace='posts')),
-    path('category/', include('blog.urls', namespace='category')),
     path('pages/', include('pages.urls', namespace='pages')),
 ]
